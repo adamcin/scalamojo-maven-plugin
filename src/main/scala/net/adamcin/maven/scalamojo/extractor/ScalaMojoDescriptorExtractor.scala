3e791e9733c96e38f3fc3cb459a9a771d6819b2e
Mark Adamcin <adamcin@gmail.com>
2012-12-11 19:30:29 -0800

    a lot of updates: - added copyright - configured pom for site deployment to https://adamcin.github.com/vltpack-maven-plugin/  - reverted to java-annotations with plan to write own scala extractor

--- src/main/scala/net/adamcin/maven/scalamojo/extractor/ScalaMojoDescriptorExtractor.scala
+++ src/main/scala/net/adamcin/maven/scalamojo/extractor/ScalaMojoDescriptorExtractor.scala
@@ -9,18 +9,41 @@ import tools.nsc.doc.{Settings, DocFactory}
 import tools.nsc.reporters.ConsoleReporter
 import tools.nsc.util.FakePos
 import java.util.Collections
+import org.codehaus.plexus.component.annotations.{Component, Requirement}
+import collection.JavaConversions
+import org.slf4j.LoggerFactory
 
 /**
  *
  * @version $Id: ScalaMojoDescriptorExtractor.java$
  * @author madamcin
  */
+@Component(role = classOf[MojoDescriptorExtractor], hint = "java-annotations-for-scala")
 class ScalaMojoDescriptorExtractor extends MojoDescriptorExtractor {
+  val log = LoggerFactory.getLogger(getClass)
+
+  /**
+   * bound requirement
+   */
+  @Requirement(role = classOf[MojoDescriptorExtractor], hint = "java-annotations")
+  var javaAnnotationsExtractor: MojoDescriptorExtractor = null
 
   @deprecated
-  def execute(p1: MavenProject, p2: PluginDescriptor) = null
+  def execute(project: MavenProject, pluginDescriptor: PluginDescriptor): java.util.List[MojoDescriptor] = {
+    val descriptors = javaAnnotationsExtractor.execute(project, pluginDescriptor)
+    JavaConversions.collectionAsScalaIterable(descriptors).foreach {
+      (descriptor) => log.info("[execute(MavenProject, PluginDescriptor)] Got descriptor: {}", descriptor.getGoal)
+    }
+    descriptors
+  }
 
   def execute(request: PluginToolsRequest): java.util.List[MojoDescriptor] = {
+    val descriptors = javaAnnotationsExtractor.execute(request)
+    JavaConversions.collectionAsScalaIterable(descriptors).foreach {
+      (descriptor) => log.info("[execute(PluginToolsRequest)] Got descriptor: {}", descriptor.getGoal)
+    }
+    descriptors
+    /*
     val sources = new File(request.getProject.getBuild.getSourceDirectory)
 
     var reporter: MojoReporter = null
@@ -30,5 +53,6 @@ class ScalaMojoDescriptorExtractor extends MojoDescriptorExtractor {
     //new DocFactory(reporter, docSettings) document command.files
 
     Collections.emptyList[MojoDescriptor]
+    */
   }
 }
