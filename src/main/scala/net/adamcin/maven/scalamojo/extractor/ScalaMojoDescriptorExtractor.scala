5f16365b960a59c6a5e9c62df431b300a1ec53f3
Mark Adamcin <adamcin@gmail.com>
2012-12-14 19:48:40 -0800

    minor changes

--- src/main/scala/net/adamcin/maven/scalamojo/extractor/ScalaMojoDescriptorExtractor.scala
+++ src/main/scala/net/adamcin/maven/scalamojo/extractor/ScalaMojoDescriptorExtractor.scala
@@ -46,9 +46,16 @@ import org.codehaus.plexus.component.annotations.{Requirement, Component}
 
 object ScalaMojoDescriptorExtractor {
   final val ROLE = classOf[MojoDescriptorExtractor]
-  final val ROLE_HINT = "java-annotations-for-scala"
+  final val ROLE_HINT = "java-annotations-and-scaladoc"
 }
 /**
+ * MojoDescriptorExtractor implementation that delegates to the OOTB JavaAnnotationsExtractor
+ * (<code>&lt;extractor&gt;java-annotations&lt;/extractor&gt;</code>) to extract
+ * all the details it can from the standard java annotations, which are retained
+ * in the .class files after compilation, and then decorates those extracted descriptors
+ * with the details that can only be found in the javadoc/scaladoc comments in the
+ * source files, namely: <code>deprecated</code>, <code>since</code>, and most importantly,
+ * <code>description</code>
  *
  * @since 1.0
  * @author Mark Adamcin
@@ -56,13 +63,6 @@ object ScalaMojoDescriptorExtractor {
 @Component(role = ScalaMojoDescriptorExtractor.ROLE, hint = ScalaMojoDescriptorExtractor.ROLE_HINT)
 class ScalaMojoDescriptorExtractor extends MojoDescriptorExtractor {
   val log = LoggerFactory.getLogger(getClass)
-
-  /**
-   * bound requirement
-   */
-
-  @Requirement
-  var repositorySystem: RepositorySystem = null
 
   @Requirement(role = ScalaMojoDescriptorExtractor.ROLE, hint = "java-annotations")
   var javaAnnotationsExtractor: MojoDescriptorExtractor = null
@@ -75,99 +75,13 @@ class ScalaMojoDescriptorExtractor extends MojoDescriptorExtractor {
   def execute(request: PluginToolsRequest): java.util.List[MojoDescriptor] = {
     val descriptors = javaAnnotationsExtractor.execute(request).toList
 
-    val alwaysCompile = true
     val sourceFiles = (for {
       root <- getSourceRoots(Option(request.getProject))
       if ((new File(root.toString)).isDirectory)
       source <- PluginUtils.findSources(root.toString, "**/*.scala")
     } yield root + java.io.File.separator + source).toList
 
-    if (request.getDependencies.isEmpty || sourceFiles.isEmpty) {
-      if (alwaysCompile) {
-        log.info("[execute] always compile, sources: {}", sourceFiles)
-        resolveDependenciesToRequest(request)
-        useCompiler(request, sourceFiles, descriptors)
-      } else {
-        getExistingDescriptor(request.getProject) match {
-          case Some(pluginDescriptor) => {
-            log.info("[execute] use existing descriptor")
-            pluginDescriptor.getMojos
-          }
-          case None => descriptors
-        }
-      }
-    } else {
-      log.info("[execute] compile normal")
-      useCompiler(request, sourceFiles, descriptors)
-    }
-  }
-
-  def useCompiler(request: PluginToolsRequest, sourceFiles: List[String], descriptors: List[MojoDescriptor]): java.util.List[MojoDescriptor] = {
-    val compiler = new ScalaDocExtractorCompiler(request)
-    val decorator = compiler.extractDescriptorDecorator(sourceFiles)
-    descriptors.map { decorator }
-  }
-
-  def getExistingDescriptor(project: MavenProject): Option[PluginDescriptor] = {
-    val pluginFile = new File(project.getBuild.getOutputDirectory, "META-INF/maven/plugin.xml")
-    log.debug("[getExistingDescriptor] pluginFile={}", pluginFile)
-
-    if (pluginFile.exists) {
-      val builder = new PluginDescriptorBuilder
-
-      Resource.fromFile(pluginFile).inputStream.acquireFor {
-        (stream) => builder.build(new InputStreamReader(stream, "UTF-8"))
-      } match {
-        case Left(exs) => {
-          log.error("[getExistingDescription] failed to read plugin.xml file: " + pluginFile, exs.head)
-          None
-        }
-        case Right(descriptor) => {
-          if (log.isDebugEnabled) {
-            descriptor.getMojos.foreach { log.debug("[getExistingDescriptor] mojo={}", _) }
-          }
-          Option(descriptor)
-        }
-      }
-    } else {
-      None
-    }
-  }
-
-  def resolveDependenciesToRequest(request: PluginToolsRequest) {
-    val artifactRequest = new ArtifactResolutionRequest(getRepositoryRequest(request))
-    artifactRequest.setResolveTransitively(true)
-
-    def dependencyFilter(dep: Dependency): Boolean = dep.getScope == Artifact.SCOPE_COMPILE
-    val artifacts = request.getProject.getDependencies.filter { dependencyFilter }.map {
-      (dep: Dependency) => repositorySystem.createDependencyArtifact(dep)
-    }
-
-    artifacts.foreach {
-      (artifact: Artifact) => {
-        artifactRequest.setArtifact(artifact)
-        artifactRequest.setCollectionFilter(new ScopeArtifactFilter(Artifact.SCOPE_COMPILE))
-        repositorySystem.resolve(artifactRequest)
-        request.getProject.getArtifacts.add(artifactRequest.getArtifact)
-        Option(artifactRequest.getArtifactDependencies) match {
-          case Some(deps) => request.getProject.getArtifacts.addAll(deps)
-          case None => ()
-        }
-      }
-    }
-  }
-
-  def getRepositoryRequest(request: PluginToolsRequest): RepositoryRequest = {
-    new ArtifactResolutionRequest(new RepositoryRequest {
-      def getLocalRepository = request.getLocal
-      def setRemoteRepositories(remoteRepositories: java.util.List[ArtifactRepository]) = null
-      def getRemoteRepositories = request.getRemoteRepos
-      def setForceUpdate(forceUpdate: Boolean) = null
-      def isOffline = false
-      def setLocalRepository(localRepository: ArtifactRepository) = null
-      def setOffline(offline: Boolean) = null
-      def isForceUpdate = false
-    })
+    useCompiler(request, sourceFiles, descriptors)
   }
 
   def getSourceRoots(p: Option[MavenProject]): List[String] = {
@@ -185,5 +99,11 @@ class ScalaMojoDescriptorExtractor extends MojoDescriptorExtractor {
     }
 
     roots
+  }
+
+  def useCompiler(request: PluginToolsRequest, sourceFiles: List[String], descriptors: List[MojoDescriptor]): java.util.List[MojoDescriptor] = {
+    val compiler = new ScalaDocExtractorCompiler(request)
+    val decorator = compiler.extractDescriptorDecorator(sourceFiles)
+    descriptors.map { decorator }
   }
 }
